d3304efd73e875ed7f1fe44ccc174da409645928
Vladislav.H <vkhrabrov94@gmail.com>
2022-07-15 11:39:59 +0300

    add

--- framework/common/env_vars.py
+++ framework/common/env_vars.py
@@ -9,7 +9,7 @@ def should_run_headless() -> bool:
     Checks if Chrome should run in headless mode or not.
     """
     # return bool(_get_env_var("RUN_HEADLESS") == "True")
-    return bool(_get_env_var(env1))
+    # return bool(_get_env_var(env1))
 
 
 
@@ -20,6 +20,7 @@ def _get_env_var(name, default: str = ""):
     :param default: Option for setting a default value to return if the env variable is not found.
     :return: The value of an environment variable.
     """
+    print(env1)
     if name not in os.environ and not default:
         raise ValueError(f"The {name} environment variable has not been set.")
     return os.getenv(name, default)
